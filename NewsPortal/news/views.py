081a66d980d63e37a2671fba597aca6c24c4712d
VladimirK95 <157058931+VladimirK95@users.noreply.github.com>
2024-06-23 16:35:41 +0300

    views.py

---
+++ NewsPortal/news/views.py
@@ -0,0 +1,75 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django_filters.views import FilterView
+
+from .forms import PostForm
+from .models import Post
+from .filters import PostFilter
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-creation_date'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'id'
+
+
+class PostSearch(FilterView):
+    model = Post
+    filterset_class = PostFilter
+    template_name = 'search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+    context_object_name = 'create'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if self.request.path == '/article/create/':
+            post.type = 'AR'
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
